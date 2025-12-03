c160f2ce11e51350ef4f28f305706cc9187924ef
Roshini155 <roshini.kr2024it@sece.ac.in>
2025-12-03 15:24:47 +0530

    Add files via upload

---
+++ javascript/demo1.js
@@ -0,0 +1,12 @@
+let name="demo"
+let val1=23
+let val2=34.5
+let isdataavail=true
+let nodata=null
+let a
+console.log(name)
+console.log(val1)
+console.log(val2)
+console.log(isdataavail)
+console.log(nodata)
+console.log(a)
