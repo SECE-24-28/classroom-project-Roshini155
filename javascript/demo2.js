4a400899c422f9d96c1abe3e4c416c41460ceebf
Roshini155 <roshini.kr2024it@sece.ac.in>
2025-12-09 18:22:34 +0530

    Delete javascript/demo2.js

--- javascript/demo2.js
+++
@@ -1,9 +0,0 @@
-let a=10;
-let b=45;
-let c=10;
-console.log(a>b || a===c)
-console.log( !(a<b && a===c))
-e=[12,34,45]
-result=a.push(163)
-console.log('the result,result',result)
-console.log('the array is',a)11
